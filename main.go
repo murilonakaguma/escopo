aa86b856a2f4e426e00b12885c134cc31acaaa1f
murilo nakaguma <murilo.nakaguma@aluno.senai.br>
2025-04-24 16:04:13 -0300

    exemplo 1

--- main.go
+++ main.go
@@ -8,5 +8,8 @@ import (
 func main(){
 	a := 10
 	b := 5
-	fmt.Println(soma(a,b))
+	fmt.Println("soma:",soma(a,b))
+	fmt.Println("subtração",subtrair(a,b))
+	fmt.Println("multiplicação",multiplicacao(a,b))
+	fmt.Println("divisão:",divisao(a,b))
 }
