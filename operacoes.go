bf44f743b422e64752d8156dc28a04d020583152
murilo nakaguma <murilo.nakaguma@aluno.senai.br>
2025-04-24 15:55:41 -0300

    escopo

---
+++ operacoes.go
@@ -0,0 +1,13 @@
+package main 
+func soma(x, y int) int {
+	return x + y
+}
+func subtrair(x, y int) int{
+	return x - y
+}
+func multiplicacao(x, y int) int{
+	return x * y
+}
+func divisao(x, y int) int{
+	return x / y
+}
